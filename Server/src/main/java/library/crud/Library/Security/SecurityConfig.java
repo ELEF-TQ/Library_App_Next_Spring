b0b9467af481b4b1a644906af1c854f6e4b40736
ELEF-TQ <tariq75aitlahklift@gmail.com>
2023-11-03 23:33:51 +0100

    spring security config

---
+++ Server/src/main/java/library/crud/Library/Security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package library.crud.Library.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeHttpRequests ->
+                    authorizeHttpRequests
+                            .anyRequest().authenticated()
+            )
+
+                .httpBasic(httpBasic -> httpBasic
+                        .realmName("Library API"));
+
+        return http.build();
+    }
+}
